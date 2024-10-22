0c40ac1097b31007418be22a9770a9de8ec3a251
heemes <semenluts90@gmail.com>
2024-10-22 21:29:43 +0600

    all good

--- main.go
+++ main.go
@@ -2,45 +2,12 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"strconv"
 	"unicode"
 )
 
-func Calc(expression string) (float64, error) {
-
-	// Parse
-	a := []string{}
-	num := ""
-	for i := 0; i < len(expression); i++ {
-		if unicode.IsDigit(rune(expression[i])) {
-			num += string(expression[i])
-		} else {
-			if num != "" {
-				a = append(a, num)
-				num = ""
-			}
-			switch expression[i] {
-			case '(':
-				a = append(a, "(")
-			case ')':
-				a = append(a, ")")
-			case '+':
-				a = append(a, "+")
-			case '-':
-				a = append(a, "-")
-			case '/':
-				a = append(a, "/")
-			case '*':
-				a = append(a, "*")
-			default:
-			}
-		}
-	}
-	if num != "" {
-		a = append(a, num)
-	}
-	// end of Parse
-
+func CalcSimple(a []string) (float64, error) {
 	for i := 0; i < len(a); i++ {
 		switch a[i] {
 		case "/":
@@ -78,12 +45,80 @@ func Calc(expression string) (float64, error) {
 			i -= 2
 		}
 	}
-
 	return strconv.ParseFloat(a[len(a)-1], 64)
+}
+
+func ParserForCalc(expression string) []string {
+	answer := []string{}
+	num := ""
+	for i := 0; i < len(expression); i++ {
+		if unicode.IsDigit(rune(expression[i])) {
+			num += string(expression[i])
+		} else {
+			if num != "" {
+				answer = append(answer, num)
+				num = ""
+			}
+			switch expression[i] {
+			case '(':
+				answer = append(answer, "(")
+			case ')':
+				answer = append(answer, ")")
+			case '+':
+				answer = append(answer, "+")
+			case '-':
+				answer = append(answer, "-")
+			case '/':
+				answer = append(answer, "/")
+			case '*':
+				answer = append(answer, "*")
+			default:
+			}
+		}
+	}
+	if num != "" {
+		answer = append(answer, num)
+	}
+	return answer
+}
+
+func IndexRight(str []string, s string) int {
+	for i := len(str) - 1; i > -1; i-- {
+		if str[i] == s {
+			return i
+		}
+	}
+	return -1
+}
+
+func Calc(expression string) (float64, error) {
+
+	// Parse
+	a := ParserForCalc(expression)
+	// end of Parse
+
+	for i := range a {
+		fmt.Println(a[i])
+	}
+	// ToDo
+	for i := slices.Index(a, ")"); i != -1; i = slices.Index(a, ")") {
+		fmt.Println("-------", i, "----------")
+
+		j := IndexRight(a[:i], "(")
+		fmt.Println("-------", j, "----------")
+
+		num, _ := CalcSimple(a[j+1 : i])
+		fmt.Println(a[j:i], num)
+		a = append(append(a[:j], fmt.Sprintf("%f", num)), a[i+1:]...)
+		// Calc
+	}
+
+	return CalcSimple(a)
 
 }
 
 func main() {
-	fmt.Println(Calc("3 * 2 + 2 * 2 / 2"))
-	fmt.Println(Calc("555 + 1 * 2"))
+	//fmt.Println(Calc("3 * 2 + 2 * 2 / 2"))
+	//fmt.Println(Calc("555 + 1 * 2"))
+	//fmt.Println(Calc("(12 + 24 * 3 * (45 - 44))"))
 }
